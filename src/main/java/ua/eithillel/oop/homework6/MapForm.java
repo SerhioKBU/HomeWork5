0d156db4bb7dcaa8fd32a53e27b45bba52a01953
Serhii <87563763+SerhioKBU@users.noreply.github.com>
2021-08-23 17:27:40 +0300

    Delete src/main/java/ua/eithillel/oop/homework6 directory

--- src/main/java/ua/eithillel/oop/homework6/MapForm.java
+++
@@ -1,9 +0,0 @@
-package ua.eithillel.oop.homework6;
-
-public interface MapForm<K,V> {
-    boolean put(K key, V value);
-    boolean containsKey(K key);
-    V get(K key);
-    K keySet();
-    int size();
-}
